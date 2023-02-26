af34f06a9d382290cfc45e5368ca402581d760b3
= <=>
2023-02-26 17:43:37 +0100

    finish rendering list vid

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0
-    }
-    render() { 
-        return <div>
-            <span className='badge bg-primary m-2'>{this.formatCount()}</span>
-            <button>Increment</button>
-        </div>;
-    }
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? <h1>Zero</h1> : count
-    }
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
 }
- 
 
 export default Counter;
