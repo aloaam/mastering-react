37a87bf9771667aba8128a695fe039c07c12e853
aloaam <aloaam@gmail.com>
2023-03-03 12:33:08 +0100

    bring mbpro up-to-date and finish section Passing Data to Children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,31 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.value,
   };
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
+  handleIncrement = () => {
+    this.setState({value: this.state.value + 1})
+  }
+
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? <span>Zero</span> : value;
   }
 }
 
